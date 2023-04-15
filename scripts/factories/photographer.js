27472f755234a0c5e0943cfd3e87f94f2e260272
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-04-15 10:58:29 +0200

    Backup - safeguarding the ongoing reorganization

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,19 @@
-function photographerFactory(data) {
+// eslint-disable-next-line no-unused-vars
+const photographerFactory = data => {
     const { name, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+    const getUserCardDOM = () => {
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute('src', picture);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
-}
+        return article;
+    };
+
+    return { name, picture, getUserCardDOM };
+};
