d7c3e84eafc97dc203b96852b7a4e8ad1a85b98e
Citizen syd <guillard.fab@pm.me>
2023-04-20 21:40:51 +0200

    Update - Step 3 - Integrate the home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,21 +1,29 @@
+let tabIndex = 3;
 const photographerFactory = data => {
-    const { name, city, tagline, price, portrait } = data;
-
+    const { name, id, city, country, tagline, price, portrait } = data;
+    
     const picture = `assets/photographers/${portrait}`;
-
+    
     const getUserCardDOM = () => {
         const article = document.createElement('article');
+        article.classList.add(`photographer-${id}`);
+        article.setAttribute("aria-label", `Profil du photographe ${name}`);
+        article.setAttribute("tabindex", tabIndex);
+        tabIndex++;
+        const containerPicture = document.createElement('div');
+        containerPicture.classList.add('photographer-icon');
         const photographerPicture = document.createElement('img');
         photographerPicture.setAttribute('src', picture);
         const photographerName = document.createElement('h2');
         photographerName.textContent = name;
-        const photographerLocation = document.createElement('h3');
-        photographerLocation.textContent = city; 
+        const photographerLocation = document.createElement('p');
+        photographerLocation.textContent = `${city}, ${country}`;
         const photographerTagline = document.createElement('p');
         photographerTagline.textContent = tagline;
         const photographerPrice = document.createElement('p');
-        photographerPrice.textContent = price;
-        article.appendChild(photographerPicture);
+        photographerPrice.textContent = `${price} €/jour`;
+        article.appendChild(containerPicture);
+        containerPicture.appendChild(photographerPicture);
         article.appendChild(photographerName);
         article.appendChild(photographerLocation); 
         article.appendChild(photographerTagline);
